11bbcc48dfc84d1dac5793f35f00cb9bdf4fda44
Amarjeet Rai <jatt27@gmail.com>
2020-01-04 14:48:22 +0000

    Add twitter meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,9 @@ const Layout = props => (
       <title key="title">Sonu Rai</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
+      <meta name="twitter:site" content="@sonu27" />
+      <meta name="twitter:creator" content="@sonu27" />
     </Head>
     <Header />
     <div className="container-lg px-0">{props.children}</div>
