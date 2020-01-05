266f3001e6854136c68e9c35553dcfc8989caf5b
Amarjeet Singh Rai <git@arai.dev>
2020-01-05 11:12:39 +0000

    Make into web app

--- components/Layout.js
+++ components/Layout.js
@@ -15,6 +15,9 @@ const Layout = props => (
       <meta name="theme-color" content="#000000" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
       <link rel="stylesheet" href="/css/index.css" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/logo192.png" />
+      <link rel="manifest" href="/manifest.json" />
       <meta name="twitter:site" content="@sonu27" />
       <meta name="twitter:creator" content="@sonu27" />
     </Head>
