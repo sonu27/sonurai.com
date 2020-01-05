8ec814153c8396798ff6632b03a450a0ceb62fc0
Amarjeet Singh Rai <git@arai.dev>
2020-01-05 09:50:05 +0000

    Add 404s

--- pages/bingwallpapers/[id].js
+++ pages/bingwallpapers/[id].js
@@ -5,26 +5,37 @@ import Api from '../../libs/Api'
 
 const apiClient = new Api()
 
-const Wallpaper = props => (
-  <Layout>
-    <Head>
-      <title key="title">{props.wallpaper.desc} - Bing Wallpapers - Sonu Rai</title>
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="og:title" content={`${props.wallpaper.desc} - Bing Wallpapers - Sonu Rai}`} />
-      <meta property="og:description" content={`${props.wallpaper.desc} ${props.wallpaper.copyright}`} />
-      <meta property="og:image" content={`https://images.sonurai.com/${props.wallpaper.name}_th.jpg`} />
-    </Head>
-    <h1 className="title px-3 px-lg-0">{props.wallpaper.desc}</h1>
-    <a href={`https://images.sonurai.com/${props.wallpaper.name}.jpg`}><img className="img-fluid" src={`https://images.sonurai.com/${props.wallpaper.name}.jpg`} alt={props.wallpaper.desc}/></a>
-    <p className="px-3 px-lg-0">{props.wallpaper.copyright} - {moment(props.wallpaper.date, 'YYYYMMDD').format('MMMM Do YYYY')}</p>
-  </Layout>
-)
+const Wallpaper = ({ wallpaper }) => {
+  const { desc, name, copyright, date } = wallpaper
+  return (
+    <Layout>
+      <Head>
+        <title key="title">{desc} - Bing Wallpapers - Sonu Rai</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:title" content={`${desc} - Bing Wallpapers - Sonu Rai}`} />
+        <meta property="og:description" content={`${desc} ${copyright}`} />
+        <meta property="og:image" content={`https://images.sonurai.com/${name}_th.jpg`} />
+      </Head>
+      <h1 className="title px-3 px-lg-0">{desc}</h1>
+      <a href={`https://images.sonurai.com/${name}.jpg`}>
+        <img className="img-fluid" src={`https://images.sonurai.com/${name}.jpg`} alt={desc}/>
+      </a>
+      <p className="px-3 px-lg-0">{copyright} - {moment(date, 'YYYYMMDD').format('MMMM Do YYYY')}</p>
+    </Layout>
+  )
+}
 
-Wallpaper.getInitialProps = async function(context) {
-  const { id } = context.query
-
+Wallpaper.getInitialProps = async function({ query, res }) {
   try {
+    const { id } = query
     const data = await apiClient.getWallpaper(id)
+
+    if (!data.wallpaper) {
+      res.statusCode = 404
+      res.end('Not found')
+      return
+    }
+
     return { wallpaper: data.wallpaper }
   } catch (err) {
     console.log(err)
