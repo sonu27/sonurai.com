0a826df9099b2e615e309ce76835cef8c7c95ed0
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2020-01-05 09:56:25 +0000

    Clean up

--- pages/bingwallpapers/page/[page].js
+++ pages/bingwallpapers/page/[page].js
@@ -12,14 +12,14 @@ const handlePageClick = (page) => {
   Router.push(`/bingwallpapers/page/${page}`)
 }
 
-const Wallpapers = props => (
+const Wallpapers = ({ page, wallpapers, pagination }) => (
   <Layout>
     <Head>
-      <title key="title">Bing Wallpapers - Page {props.page} - Sonu Rai</title>
+      <title key="title">Bing Wallpapers - Page {page} - Sonu Rai</title>
     </Head>
     <h1 className="title px-3 px-lg-0">Bing Wallpapers</h1>
-    {props.wallpapers.map((wallpaper) => (
-      <div className="wallpaper">
+    {wallpapers.map((wallpaper) => (
+      <div key={wallpaper.id} className="wallpaper">
         <h3 className="px-3 px-lg-0">{wallpaper.desc}</h3>
         <Link href="/bingwallpapers/[id]" as={`/bingwallpapers/${wallpaper.id}`}>
           <a><img className="img-fluid" src={`https://images.sonurai.com/${wallpaper.name}_th.jpg`} alt={wallpaper.desc}/></a>
@@ -27,14 +27,13 @@ const Wallpapers = props => (
         <div className="px-3 px-lg-0">{moment(wallpaper.date, 'YYYYMMDD').format('MMMM Do YYYY')}</div>
       </div>
     ))}
-    <Pagination pagination={props.pagination} handlePageChange={handlePageClick} />
+    <Pagination pagination={pagination} handlePageChange={handlePageClick} />
   </Layout>
 )
 
-Wallpapers.getInitialProps = async function(context) {
-  const { page } = context.query
-
+Wallpapers.getInitialProps = async function({ query, res }) {
   try {
+    const { page } = query
     const data = await apiClient.getWallpapers(page)
     return {
       page: page,
