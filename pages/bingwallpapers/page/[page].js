e1a560424da325fa0e6825a4b5a49cdd0fe4795c
Amarjeet Rai <jatt27@gmail.com>
2020-01-09 18:45:02 +0000

    Move away from algolia

--- pages/bingwallpapers/page/[page].js
+++ pages/bingwallpapers/page/[page].js
@@ -13,13 +13,13 @@ const Wallpapers = ({ page, wallpapers, pagination }) => (
       <title key="title">Bing Wallpapers - Page {page} - Sonu Rai</title>
     </Head>
     <h1 className="title px-3 px-lg-0">Bing Wallpapers</h1>
-    {wallpapers.map((wallpaper) => (
-      <div key={wallpaper.id} className="wallpaper">
-        <h3 className="px-3 px-lg-0">{wallpaper.desc}</h3>
-        <Link href="/bingwallpapers/[id]" as={`/bingwallpapers/${wallpaper.id}`}>
-          <a><img className="img-fluid" src={`https://images.sonurai.com/${wallpaper.name}_th.jpg`} alt={wallpaper.desc}/></a>
+    {wallpapers.map(({ id, title, date, filename }) => (
+      <div key={id} className="wallpaper">
+        <h3 className="px-3 px-lg-0">{title}</h3>
+        <Link href="/bingwallpapers/[id]" as={`/bingwallpapers/${id}`}>
+          <a><img className="img-fluid" src={`https://images.sonurai.com/${filename}_th.jpg`} alt={title}/></a>
         </Link>
-        <div className="px-3 px-lg-0">{moment(wallpaper.date, 'YYYYMMDD').format('MMMM Do YYYY')}</div>
+        <div className="px-3 px-lg-0">{moment(date, 'YYYYMMDD').format('MMMM Do YYYY')}</div>
       </div>
     ))}
     <Pagination pagination={pagination} />
