8c5df0c032d1fb0cd66b21114efc3a72ec3b9601
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2020-02-15 19:58:32 +0000

    Change blog link

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = () => (
       <h1>Home</h1>
       <ul>
         <li><Link href="/bingwallpapers/page/[...p]" as="/bingwallpapers/page/1"><a>Bing Wallpapers</a></Link></li>
-        <li><a href="http://blog.sonurai.com/" rel="me">Tumblr Blog</a></li>
+        <li><a href="https://blog.sonurai.com" rel="me">Tumblr Blog</a></li>
       </ul>
     </div>
   </Layout>
