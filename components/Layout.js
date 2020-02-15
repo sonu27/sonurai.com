7877d17d8cb09b7d6c359d82cf9aa7dcfad054d7
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2020-02-15 20:22:17 +0000

    Compile CSS to fix caching issues

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,6 @@ const Layout = props => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
-      <link rel="stylesheet" href="/css/index.css" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/logo192.png" />
       <link rel="manifest" href="/manifest.json" />
