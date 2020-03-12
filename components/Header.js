d8fca637a8e9aece7e9713ff4670e7dd7d461862
Amarjeet Singh Rai <git@arai.dev>
2020-03-12 14:36:16 +0000

    Transition title to Japanese on hover

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,14 @@ import Link from 'next/link'
 const Header = () => (
   <header className="navbar navbar-expand navbar-dark bg-dark">
     <nav className="container-lg">
-      <Link href="/"><a className="navbar-brand">Sonu Rai</a></Link>
+      <div id="site-title">
+        <Link href="/">
+          <a className="navbar-brand">
+            <span className="t">Sonu Rai</span>
+            <span>ソヌライ</span>
+          </a>
+        </Link>
+      </div>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
