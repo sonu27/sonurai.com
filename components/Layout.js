2eb33f79655e69ea8bf5941f5ca8ad059d132c2e
Amarjeet Singh Rai <git@arai.dev>
2020-05-11 21:02:24 +0100

    Move header into layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,5 @@
 import Head from 'next/head'
-import Header from './Header'
-
-const Footer = props => (
-  <footer className="container-lg px-0">
-    <div className="px-3 px-lg-0">&copy; 2013-{new Date().getFullYear()} Amarjeet Rai</div>
-  </footer>
-)
+import Link from 'next/link'
 
 const Layout = props => (
   <div>
@@ -24,6 +18,37 @@ const Layout = props => (
     <div className="container-lg px-0">{props.children}</div>
     <Footer />
   </div>
+)
+
+const Header = () => (
+  <header className="navbar navbar-expand navbar-dark bg-dark">
+    <nav className="container-lg">
+      <div id="site-title">
+        <Link href="/">
+          <a className="navbar-brand">
+            <span className="t">Sonu Rai</span>
+            <span>ソヌライ</span>
+          </a>
+        </Link>
+      </div>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link href="/"><a className="nav-item nav-link">Home</a></Link>
+          <Link href="/about"><a className="nav-item nav-link">About</a></Link>
+          <Link href="/contact"><a className="nav-item nav-link">Contact</a></Link>
+        </div>
+      </div>
+    </nav>
+  </header>
+)
+
+const Footer = props => (
+  <footer className="container-lg px-0">
+    <div className="px-3 px-lg-0">&copy; 2013-{new Date().getFullYear()} Amarjeet Rai</div>
+  </footer>
 )
 
 export default Layout
