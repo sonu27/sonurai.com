48893107303b74303342217fd358733f6027b93f
Amarjeet Singh Rai <git@arai.dev>
2020-11-01 10:47:54 +0000

    Remove Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 import App from 'next/app'
-import Router from 'next/router'
-import * as gtag from 'libs/gtag'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'css/index.css'
-
-Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 export default App
