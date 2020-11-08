7e163804c11427990571428d9d75ce154fdd30f7
Amarjeet Rai <jatt27@gmail.com>
2020-11-08 11:43:33 +0000

    Use getServerSideProps instead of getInitialProps

--- pages/bingwallpapers/page/[...p].js
+++ pages/bingwallpapers/page/[...p].js
@@ -17,70 +17,68 @@ const Pagination = ({ pagination }) => (
   </ul>
 )
 
-const Wallpapers = ({ page, wallpapers, pagination }) => (
-  <Layout>
-    <Head>
-      <title key="title">Bing Wallpapers - Sonu Rai</title>
-    </Head>
-    <h1 className="px-3 px-lg-0">Bing Wallpapers</h1>
-    {wallpapers.map(({ id, title, date, filename }) => (
-      <div key={id} className="wallpaper">
-        <h2 className="px-3 px-lg-0">{title}</h2>
-        <Link href={`/bingwallpapers/${id}`}>
-          <a>
-            <Image
-              className="img-fluid"
-              src={`https://images.sonurai.com/${filename}_th.jpg`}
-              width="1920"
-              height="1080"
-              alt={title}
-            />
-          </a>
-        </Link>
-        <div className="px-3 px-lg-0">{intToDate(date)}</div>
-      </div>
-    ))}
-    <Pagination pagination={pagination} />
-  </Layout>
-)
+export default function Wallpapers({ page, wallpapers, pagination }) {
+  return (
+    <Layout>
+      <Head>
+        <title key="title">Bing Wallpapers - Sonu Rai</title>
+      </Head>
+      <h1 className="px-3 px-lg-0">Bing Wallpapers</h1>
+      {wallpapers.map(({ id, title, date, filename }) => (
+        <div key={id} className="wallpaper">
+          <h2 className="px-3 px-lg-0">{title}</h2>
+          <Link href={`/bingwallpapers/${id}`}>
+            <a>
+              <Image
+                className="img-fluid"
+                src={`https://images.sonurai.com/${filename}_th.jpg`}
+                width="1920"
+                height="1080"
+                alt={title}
+              />
+            </a>
+          </Link>
+          <div className="px-3 px-lg-0">{intToDate(date)}</div>
+        </div>
+      ))}
+      <Pagination pagination={pagination} />
+    </Layout>
+  )
+}
 
-Wallpapers.getInitialProps = async function({ query, res }) {
-  try {
-    const { p } = query
-    const page = p[0]
-    const startAfterDate = p[1]
-    const startAfterID = p[2]
-    const prev = (page === 'prev')
+export async function getServerSideProps({ query, res }) {
+  const { p } = query
+  const page = p[0]
+  const startAfterDate = p[1]
+  const startAfterID = p[2]
+  const prev = (page === 'prev')
 
-    if (page !== '1' && (!startAfterDate || !startAfterID)) {
-      if (res) {
-        res.writeHead(302, {
-          Location: '/bingwallpapers/page/1'
-        })
-        res.end()
-      } else {
-        Router.push('/bingwallpapers/page/1')
-      }
+  if (page !== '1' && (!startAfterDate || !startAfterID)) {
+    if (res) {
+      res.writeHead(302, {
+        Location: '/bingwallpapers/page/1'
+      })
+      res.end()
+    } else {
+      Router.push('/bingwallpapers/page/1')
     }
+  }
 
-    const data = await apiClient.getWallpapers(startAfterDate, startAfterID, prev)
+  const data = await apiClient.getWallpapers(startAfterDate, startAfterID, prev)
 
-    if (data.wallpapers.length === 0) {
-      if (res) {
-        res.statusCode = 404
-        res.end('Not found')
-      }
-      return
+  if (data.wallpapers.length === 0) {
+    if (res) {
+      res.statusCode = 404
+      res.end('Not found')
     }
+    return
+  }
 
-    return {
+  return {
+    props: {
       page: page,
       wallpapers: data.wallpapers,
       pagination: data.pagination,
     }
-  } catch (err) {
-    console.log(err)
   }
 }
-
-export default Wallpapers
