92ee01273a6253914b4ea79308a5d4966d40d05f
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2020-11-11 14:36:48 +0000

    Change links to new blog

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Index = () => (
       <h1>Home</h1>
       <ul>
         <li><Link href="/bingwallpapers"><a>Bing Wallpapers</a></Link></li>
-        <li><a href="https://blog.sonurai.com" rel="me">Tumblr Blog</a></li>
+        <li><a href="https://arai.dev" rel="me">Blog</a></li>
       </ul>
     </div>
   </Layout>
