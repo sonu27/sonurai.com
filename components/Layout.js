7ffb9b5d32794ef28877c9600e9e0e415ea64993
Amarjeet Singh Rai <jatt27@gmail.com>
2021-01-01 15:13:51 +0000

    Change import

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import ActiveLink from './ActiveLink'
+import ActiveLink from 'components/ActiveLink'
 
 const Layout = (props) => (
   <div>
