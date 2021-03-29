71415831caf5afec9e43892194665c58532d14ed
Amarjeet Singh Rai <git@arai.dev>
2021-03-29 10:54:37 +0100

    Launch new layout and colour scheme

--- pages/bingwallpapers/tags/[tag].js
+++ pages/bingwallpapers/tags/[tag].js
@@ -3,33 +3,32 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Layout from 'components/Layout'
 import client from 'libs/Client'
-import { intToDate } from 'libs/date'
 
 export default function Wallpapers({ wallpapers, tag }) {
   return (
     <Layout>
       <Head>
-        <title key="title">{tag} - Bing Wallpapers - {process.env.NEXT_PUBLIC_NAME}</title>
+        <title key="title">Tagged &quot;{tag}&quot; - Bing Wallpapers - {process.env.NEXT_PUBLIC_NAME}</title>
         <meta name="description" content="Bing Wallpapers" />
       </Head>
-      <h1 className="px-3 px-lg-0">{tag} - Bing Wallpapers</h1>
-      {wallpapers.map(({ id, title, date, filename }) => (
-        <div key={id} className="wallpaper">
-          <h2 className="px-3 px-lg-0">{title}</h2>
-          <Link href={`/bingwallpapers/${id}`}>
-            <a title={title}>
-              <Image
-                className="img-fluid"
-                src={`https://images.sonurai.com/${filename}_th.jpg`}
-                width="1920"
-                height="1080"
-                alt={title}
-              />
-            </a>
-          </Link>
-          <div className="px-3 px-lg-0">{intToDate(date)}</div>
-        </div>
-      ))}
+      <h1 className="px-3 px-lg-0">Search results for: &quot;{tag}&quot;</h1>
+      <div className="flex-container">
+        {wallpapers.map(({ id, title, filename }) => (
+          <div key={id} className="wallpaper flex-item">
+            <Link href={`/bingwallpapers/${id}`}>
+              <a title={title}>
+                <Image
+                  className="img-fluid"
+                  src={`https://images.sonurai.com/${filename}.jpg`}
+                  width="1920"
+                  height="1200"
+                  alt={title}
+                />
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 }
