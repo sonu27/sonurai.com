666ca075ff8ce166607568db3cc535e69bd79bb8
Amarjeet Singh Rai <jatt27@gmail.com>
2021-07-19 21:12:29 +0100

    Add image limit and stop using image optimisation [Vercel over limit temp fix]

--- components/WallpaperList.js
+++ components/WallpaperList.js
@@ -1,24 +1,22 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function WallpaperList({ wallpapers }) {
   return (
-    <div className="flex-container">
+    <div>
       {wallpapers.map(({ id, title, filename }) => (
-        <figure key={id} className="wallpaper flex-item">
+        <figure key={id}>
           <Link href={`/bingwallpapers/${id}`}>
             <a title={title}>
-              <Image
-                layout="responsive"
+              <img
                 className="img-fluid"
                 src={`https://images.sonurai.com/${filename}.jpg`}
                 width="1920"
                 height="1200"
                 alt={title}
               />
-              <figcaption className="caption">{title}</figcaption>
             </a>
           </Link>
+          <figcaption>{title}</figcaption>
         </figure>
       ))}
     </div>
