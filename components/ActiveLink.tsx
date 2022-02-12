e36d9bf376e958fd5ff1dadb3e9f1de9adc6c072
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2022-02-12 11:55:29 +0000

    fix

--- components/ActiveLink.tsx
+++ components/ActiveLink.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function ActiveLink({ href, children }: { href: string }) {
+export default function ActiveLink({ href, children }: { href: string, children: any }) {
   const router = useRouter()
 
   let className = children.props.className || ''
