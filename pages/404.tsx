a9f84d01d0bca7a6868d5fc4080349846bdffff3
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2022-02-12 13:20:37 +0000

    add friendly 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+const Custom404 = () => (
+  <Layout>
+    <div className="about mx-2 md:mx-0">
+    <h1 className="text-white text-2xl">404 - Page Not Found</h1>
+    </div>
+  </Layout>
+)
+
+export default Custom404
