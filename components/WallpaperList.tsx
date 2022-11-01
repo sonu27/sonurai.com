0457743c22e39e9b2aa4fc56746579cd698f1278
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2022-11-01 11:27:36 +0000

    remove lazy loading for first 3 images

--- components/WallpaperList.tsx
+++ components/WallpaperList.tsx
@@ -5,13 +5,14 @@ import { Wallpaper } from '../libs/Client'
 export default function WallpaperList({ wallpapers } : { wallpapers: Wallpaper[] }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
-      {wallpapers.map(({ id, title, filename }) => (
+      {wallpapers.map(({ id, title, filename }, i) => (
         <figure key={id} className="wallpaper relative">
           <Link href={`/bingwallpapers/${id}`} title={title} passHref>
             <Image
               src={`https://images.sonurai.com/${filename}.jpg`}
               width={1920}
               height={1200}
+              priority={i < 3 ? true : false}
               alt={title}
             />
             <figcaption className="caption hidden absolute bottom-0 left-0 p-4 h-full w-full text-2xl bg-black bg-opacity-80 text-white">{title}</figcaption>
