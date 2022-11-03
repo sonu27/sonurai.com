af97d309c17a6e69799c485e68c9b7b5bd44f2ab
Amarjeet Singh Rai <git@arai.dev>
2022-11-03 15:04:25 +0000

    improve tag cloud

--- pages/bingwallpapers/tags/index.tsx
+++ pages/bingwallpapers/tags/index.tsx
@@ -4,10 +4,11 @@ import Layout from '../../../components/Layout'
 
 export default function T({ tags }: { tags: [string, number][] }) {
   const max = tags.reduce((a, c) => Math.max(a, c[1]), 0)
+  const min = tags.reduce((a, c) => Math.min(a, c[1]), max)
   const tagFields = tags.map((l, i) => {
-    const normalised = (l[1] - 1) / (max - 1) * 4 + 1
+    const normalised = (l[1] - min) / (max - min) * 5 + 1
     return (
-      <Fragment key={i}><Link href={`/bingwallpapers/tags/${l[0]}`} className="px-3 py-2 rounded-md text-gray-300 hover:bg-slate-700 hover:text-white" style={{"fontSize": normalised+"em"}}>{l[0]}</Link> </Fragment>
+      <Fragment key={i}><Link href={`/bingwallpapers/tags/${l[0]}`} className="px-2 py-1 leading-[3rem] rounded-md text-gray-300 hover:bg-slate-700 hover:text-white" style={{"fontSize": normalised+"em"}}>{l[0]}</Link> </Fragment>
     )
   })
 
@@ -15,7 +16,7 @@ export default function T({ tags }: { tags: [string, number][] }) {
   return (
     <Layout pageTitle={pageTitle}>
       <h1 className="text-3xl mb-2 text-white mx-4 md:mx-0">Tags - Bing Wallpapers</h1>
-      {tagFields}
+      <div className="mx-2 md:mx-0">{tagFields}</div>
     </Layout>
   )
 }
