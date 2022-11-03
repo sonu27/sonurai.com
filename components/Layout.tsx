f25bb42803656bf7b1fb118e798b74cc8ae1fbf6
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2022-11-03 12:26:09 +0000

    add more functionality on tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,24 +1,25 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import ActiveLink from '../components/ActiveLink'
 
-const Layout = (props: any) => (
-  <div className="container mx-auto">
-    <Head>
-      <title key="title">{process.env.NEXT_PUBLIC_NAME}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#111827" />
-      <link rel="icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" href="/logo192.png" />
-      <link rel="manifest" href="/manifest.json" />
-      <meta name="twitter:site" content="@sonu27" />
-      <meta name="twitter:creator" content="@sonu27" />
-    </Head>
-    <Header />
-    {props.children}
-    <Footer />
-  </div>
-)
+export default function RootLayout (props: any) {
+  return (
+    <div className="container mx-auto">
+      <Head>
+        <title key="title">{process.env.NEXT_PUBLIC_NAME}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#111827" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="twitter:site" content="@sonu27" />
+        <meta name="twitter:creator" content="@sonu27" />
+      </Head>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  )
+}
 
 const Header = () => (
   <nav className="flex items-center h-16 mx-2 md:mx-0" aria-label="navigation">
@@ -40,5 +41,3 @@ const Header = () => (
 const Footer = () => (
   <footer className="mt-4 mb-16 mx-2 md:mx-0 text-gray-400">&copy; 2013-{new Date().getFullYear()} Amarjeet Rai</footer>
 )
-
-export default Layout
