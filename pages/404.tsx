65fcbcdcf5dbf58b3b95d9a6c9ea3a7c3842fa59
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2022-11-03 15:53:57 +0000

    fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ export default function Custom404() {
   const pageTitle = `404 - Page Not Found`
   return (
     <Layout pageTitle={pageTitle}>
-      <h1 className="text-white text-2xl">{pageTitle}</h1>
+      <h1 className="text-white text-2xl mx-4 md:mx-0">{pageTitle}</h1>
     </Layout>
   )
 }
