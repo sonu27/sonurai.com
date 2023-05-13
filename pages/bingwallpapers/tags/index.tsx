c42ed64c9d08d6ec77e71224fea6f87df04b259d
Amarjeet Singh Rai <git@arai.dev>
2023-05-13 11:38:04 +0100

    fix largest tag size

--- pages/bingwallpapers/tags/index.tsx
+++ pages/bingwallpapers/tags/index.tsx
@@ -6,7 +6,7 @@ export default function T({ tags }: { tags: [string, number][] }) {
   const max = tags.reduce((a, c) => Math.max(a, c[1]), 0)
   const min = tags.reduce((a, c) => Math.min(a, c[1]), max)
   const tagFields = tags.map((l, i) => {
-    const normalised = (l[1] - min) / (max - min) * 5 + 1
+    const normalised = (l[1] - min) / (max - min) * 4.2 + 1
     return (
       <Fragment key={i}><Link prefetch={false} href={`/bingwallpapers/tags/${l[0]}`} className="px-2 py-1 leading-[3rem] rounded-md text-gray-300 hover:bg-slate-700 hover:text-white" style={{"fontSize": normalised+"em"}}>{l[0]}</Link> </Fragment>
     )
