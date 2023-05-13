c39a50384597231a19fb07f244819906b923100e
Amarjeet Singh Rai <git@arai.dev>
2023-05-13 11:35:00 +0100

    fix tag header

--- pages/bingwallpapers/tags/[tag].tsx
+++ pages/bingwallpapers/tags/[tag].tsx
@@ -21,7 +21,7 @@ export default function Wallpapers({ wallpapers, tag, nextUrl }: { wallpapers: W
   const pageTitle = `Tagged "${tag}" - Bing Wallpapers - ${process.env.NEXT_PUBLIC_NAME}`
   return (
     <Layout pageTitle={pageTitle}>
-      <h1 className="text-3xl mb-2 text-white my-4 mx-4 md:mx-0">Wallpapers tagged with &quot;{tag}&quot;</h1>
+      <h1 className="text-3xl mb-2 text-white mx-4 md:mx-0">Wallpapers tagged with &quot;{tag}&quot;</h1>
       <WallpaperList wallpapers={wallpaperList} />
       <div className="pagination my-4 mx-4 md:mx-0">
         {next !== "" && <button onClick={moreFn} className="px-3 py-2 rounded-md bg-slate-800 text-gray-300 hover:bg-slate-700 hover:text-white">
