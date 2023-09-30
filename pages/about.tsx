8efb62410e130587f99f712f1fe3ec05248f1837
Amarjeet Singh Rai <git@arai.dev>
2023-09-30 10:17:24 +0100

    add sponsorship message

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className="about mx-4 md:mx-0">
         <h1 className="text-3xl mb-2 text-white">About</h1>
         <p>Built by <a href="https://amarjeet.dev" rel="me">Amarjeet Rai</a></p>
-        <p>You can contact me on Twitter <a href="https://twitter.com/sonu27" rel="me">@sonu27</a></p>
+        <p>You can email me on sonu [at] sonurai.com</p>
         <p>
           <a href="https://arai.dev" rel="me">Blog</a><br />
           <a href="https://github.com/sonu27/" rel="me">GitHub</a><br />
